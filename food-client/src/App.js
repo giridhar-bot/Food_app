1b574676ac84dbc2bb24efc38a42bc33e7d28f29
Giridhar.kumar <giridhar.kumar@infinite.com>
2023-02-16 15:49:25 +0530

    first commit

--- food-client/src/App.js
+++ food-client/src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
-import CardProvider from "./configuration/CardProvider";
-import ProductList from "./Components/ProductList";
-import CheckoutPage from "./Components/Checkout/CheckoutPage";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./Components/Login/LoginForm";
+
+const AuthContext = React.createContext();
 
 const App = () => {
+  const [authenticated, setAuthenticated] = useState(
+    localStorage.getItem("authenticated") || false
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem("authenticated");
+    if (token) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
+  }, []);
+
   return (
-    <CardProvider>
-      <ProductList />
-      <CheckoutPage />
-    </CardProvider>
+    <AuthContext.Provider value={{ authenticated, setAuthenticated }}>
+      <Router>
+        <Routes>
+          <Route exact path="/login">
+            {authenticated ? <Navigate to="/" /> : <LoginPage />}
+          </Route>
+          <Route path="/">
+            {authenticated ? <h1>Welcome!</h1> : <Navigate to="/login" />}
+          </Route>
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
 export default App;
+
+
